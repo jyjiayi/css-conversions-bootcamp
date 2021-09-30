76afbc3391e81dd3691d24721f9f039dde04d3b5
jyjiayi <sojiayi@gmail.com>
2021-09-30 23:46:40 +0800

    Completed Base

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const hexCode = process.argv[2];
+
+const convertHexToRgb = (hex) =>{
+const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+console.log(convertHexToRgb(hexCode));
+
